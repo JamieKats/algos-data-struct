b6e9a8f85883c01073852cf3d2d2ead4aa2259f5
Jamie Katsamatsas <jamie.kats@hotmail.com>
2022-08-29 14:43:17 +1000

    Finish hospital 2

--- Java/Q1/Hospital2.java
+++ Java/Q1/Hospital2.java
@@ -11,13 +11,20 @@ public class Hospital2 extends HospitalBase {
     private String breakStart = "11:59";
     private String breakEnd = "13:00";
 
-    private final int initialArraySize = 100;
+    private final int initialArraySize = 2;
 
-    private int numAppointments = 0;
+    private int growthMultiplier;
+
+    private int appointmentsSize;
+
+    private int numAppointments;
 
     public Hospital2() {
         /* Add your code here! */
         this.appointments = new PatientBase[initialArraySize];
+        this.appointmentsSize = initialArraySize;
+        this.numAppointments = 0;
+        this.growthMultiplier = 0;
     }
 
     @Override
@@ -35,45 +42,61 @@ public class Hospital2 extends HospitalBase {
         }
 
         // Is array large enough?
-        if (numAppointments == appointments.length) { // Need to expand array
-            // increase array by amortised doubling
+        // Doubling strategy array growth
+        if (this.numAppointments == this.appointments.length) { // Need to expand array
+           growArrayDoubleStrategy();
         }
 
         // Insert patient at end of array
-        appointments[numAppointments++] = patient;
+        this.appointments[this.numAppointments++] = patient;
         System.out.println("Patient added to list");
 
 
-        if (numAppointments == 1) { // First patient has been added
+        if (this.numAppointments == 1) { // First patient has been added
             return true;
         }
 
 //        int newPatientIndex = numAppointments - 1; // New patient at last index of appointments list
 
-        for (int i = numAppointments - 2; i >= 1; i--) {
+        for (int i = this.numAppointments - 2; i >= 0; i--) {
             int newPatientIndex = i + 1;
-            if (Patient.compareTimes(appointments[i].getTime(), patient.getTime()) <= 0) {
+            if (Patient.compareTimes(this.appointments[i].getTime(), patient.getTime()) <= 0) {
                 break; // Patient is at correct position
             }
             // Swap the current patient index with the patient to the left
-            swapPatients(i, newPatientIndex, appointments);
+            swapPatients(i, newPatientIndex, this.appointments);
 //            newPatientIndex--;
-        }
+//            System.out.println("NEW REARRANGE");
 
-        for (var patient1 : appointments) {
-            if (patient1 == null) {
-                break;
-            }
-            System.out.println(patient1);
         }
+//        for (var patient1 : this.appointments) {
+//            if (patient1 == null) {
+//                break;
+//            }
+//            System.out.println(patient1);
+//        }
+
         return true;
+    }
+
+    /** Increase array size by doubling strategy to achieve O(1) amortised array growth */
+    public void growArrayDoubleStrategy() {
+        int oldAppointmentsSize = this.appointmentsSize;
+        this.appointmentsSize += Math.pow(2, this.growthMultiplier++);
+        PatientBase[] newArray = new PatientBase[this.appointmentsSize];
+
+        // Copy old array values into temp new array
+        for (int i = 0; i < oldAppointmentsSize; i++) {
+            newArray[i] = this.appointments[i];
+        }
+        this.appointments = newArray;
     }
 
     /** Swap the patients at index i and p */
     public void swapPatients(int i, int p, PatientBase[] appointments) {
         PatientBase tmp = appointments[i];
-        appointments[p] = appointments[i];
-        appointments[i] = tmp;
+        appointments[i] = appointments[p];
+        appointments[p] = tmp;
     }
 
     @Override
@@ -84,7 +107,7 @@ public class Hospital2 extends HospitalBase {
             int currIndex = 0;
             @Override
             public boolean hasNext() {
-                if (currIndex < numAppointments - 1) {
+                if (currIndex < numAppointments) {
                     return true;
                 }
                 return false;
@@ -110,29 +133,50 @@ public class Hospital2 extends HospitalBase {
          * The following main method is provided for simple debugging only
          */
         var hospital = new Hospital2();
+//        System.out.println(String.format("2^3 = %d", ));
         var p1 = new Patient("Max", "11:00");
         var p2 = new Patient("Alex", "13:15");
         var p3 = new Patient("George", "14:00");
         var p4 = new Patient("Damo", "17:00");
         var p5 = new Patient("Jamie", "08:00");
+        var p6 = new Patient("Emily", "08:01");
+        var p7 = new Patient("Bort", "08:00");
+        var p8 = new Patient("LateMan", "18:00");
         hospital.addPatient(p1);
         for (var patient: hospital) {
             System.out.println(patient);
         }
         hospital.addPatient(p2);
+        System.out.println(String.format("appointment size = %d", hospital.appointmentsSize));
         hospital.addPatient(p3);
+        System.out.println(String.format("appointment size = %d", hospital.appointmentsSize));
         hospital.addPatient(p4);
+        System.out.println(String.format("appointment size = %d", hospital.appointmentsSize));
         hospital.addPatient(p5);
+        System.out.println(String.format("appointment size = %d", hospital.appointmentsSize));
+        hospital.addPatient(p6);
+        System.out.println(String.format("appointment size = %d", hospital.appointmentsSize));
+        hospital.addPatient(p7);
+        System.out.println(String.format("appointment size = %d", hospital.appointmentsSize));
+        hospital.addPatient(p8);
+        System.out.println(String.format("appointment size = %d", hospital.appointmentsSize));
+        System.out.println(String.format("num appointments = %d", hospital.numAppointments));
         var patients = new Patient[] {p1, p2, p3};
         int i = 0;
 
-        for (var patient : hospital) {
+//        for (var patient : hospital) {
 //            System.out.println(patient);
-            assert Objects.equals(patient, patients[i++]);
+////            assert Objects.equals(patient, patients[i++]);
+//
+////            if (!Objects.equals(patient, patients[i++])) {
+////                System.err.println("Wrong patient encountered, check your implementation!");
+////            }
+//        }
 
-            if (!Objects.equals(patient, patients[i++])) {
-                System.err.println("Wrong patient encountered, check your implementation!");
-            }
+        Iterator<PatientBase> it = hospital.iterator();
+        while (it.hasNext()) {
+            PatientBase patient = it.next();
+            System.out.println(patient);
         }
     }
 }
